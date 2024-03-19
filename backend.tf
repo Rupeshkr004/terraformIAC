242b1f1dd0f9fb7b910d5f175ed070bcef51fff3
Rupeshkr004 <104455878+Rupeshkr004@users.noreply.github.com>
2024-03-19 21:26:47 +0530

    Add Azure Storage backend configuration for Terraform

--- backend.tf
+++ backend.tf
@@ -1,1 +1,9 @@
+terraform {
+  backend "azurerm" {
+    storage_account_name = "terraformbackupstfile01"
+    container_name       = "statefile-backup"
+    key                  = "terraform.tfstate"
 
+    access_key = "eOv5mwQaNd9aAGoAR1xRfNJzbsym7gFc3dzgUZiNXUCVnU67QGpfynkP29L+dMKxMR+PM+R39VKI+AStxx9NBA=="
+  }
+}
